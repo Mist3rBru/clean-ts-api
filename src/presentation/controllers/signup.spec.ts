3c41f1a0cae5925632d0ac87a8abf5c6d04b7d49
Bruno Rocha <brunorocha2674@gmail.com>
2022-03-04 17:06:46 -0300

    fix: to fix

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,3 +1,4 @@
+import { MissingParamError } from '../../utils/errors'
 import { Signup } from './signup'
 
 const makeSut = (): any => {
@@ -6,6 +7,21 @@ const makeSut = (): any => {
 }
 
 describe('Signup Controller', () => {
+  it('should return 400 if no name is provided', async () => {
+    const sut = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any-name',
+        email: 'any-email',
+        password: 'any-password',
+        password_confirmation: 'any-password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body.error).toBe(new MissingParamError('name').message)
+  })
+
   it('should return 400 if no name is provided', async () => {
     const sut = makeSut()
     const httpResponse = await sut.handle()
--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,11 @@
+import { badRequest } from '@/utils/helpers'
+import { MissingParamError } from '@/utils/errors'
 export class Signup {
   async handle (httpRequest: any): Promise<any> {
-    return {
-      statusCode: 400,
-      body: {
-        error: 'Missing param: name'
+    const requiredParams = ['name', 'email', 'password', 'password_confirmation']
+    for (const param of requiredParams) {
+      if (!httpRequest[param]) {
+        return badRequest(new MissingParamError(param))
       }
     }
   }
