5d8fa1c7d72c95959b6445cfbc2cb1ce591c8d0e
Bruno Rocha <brunorocha2674@gmail.com>
2022-03-06 04:07:30 -0300

    feat: ensure UserRepository return user when registered

--- src/infra/database/mongodb/UserRepository.spec.ts
+++ src/infra/database/mongodb/UserRepository.spec.ts
@@ -1,9 +1,18 @@
 import { UserRepository } from './UserRepository'
 import { MongoHelper } from './MongoHelper'
+import { AddUserModel } from '@/domain/usecases'
 
 const makeSut = (): UserRepository => {
   const sut = new UserRepository()
   return sut
+}
+
+const makeUser = (): AddUserModel => {
+  return {
+    name: 'any-name',
+    email: 'any-email',
+    password: 'any-password'
+  }
 }
 
 describe('UserRepository', () => {
@@ -13,5 +22,16 @@ describe('UserRepository', () => {
 
   afterAll(async () => {
     await MongoHelper.disconnect()
+  })
+
+  it('should return user when registered on MongoDB', async () => {;
+    const sut = makeSut()
+    const model = makeUser()
+    const user = await sut.add(model)
+    expect(user).toEqual({
+      id: 'any-id',
+      name: model.name,
+      email: model.email
+    })
   })
 })
