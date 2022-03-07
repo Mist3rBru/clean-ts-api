c5118d784e25c641f684ce32e0e0ac9ada9470b3
Bruno Rocha <brunorocha2674@gmail.com>
2022-03-07 12:27:25 -0300

    feat: ensure LogControllerDecorator returns Controller response

--- src/decorator/LogControllerDecorator.spec.ts
+++ src/decorator/LogControllerDecorator.spec.ts
@@ -5,6 +5,7 @@ interface SutTypes {
   sut: LogControllerDecorator
   controllerSpy: Controller
 }
+
 const makeSut = (): SutTypes => {
   const controllerSpy = new ControllerSpy()
   const sut = new LogControllerDecorator(controllerSpy)
@@ -16,25 +17,34 @@ const makeSut = (): SutTypes => {
 
 class ControllerSpy implements Controller {
   async handle (request: HttpRequest): Promise<HttpResponse> {
-    return new Promise(resolve => resolve({
+    const httpResponse: HttpResponse = {
       statusCode: 200,
-      body: {
-        ...request.body
-      }
-    }))
+      body: request.body
+    }
+    return new Promise(resolve => resolve(httpResponse))
   }
 }
+
+const makeRequest = (): HttpRequest => ({
+  body: {
+    name: 'any-name'
+  }
+})
 
 describe('LogControllerDecorator', () => {
   it('should call Controller with correct value', async () => {
     const { sut, controllerSpy } = makeSut()
     const handleSpy = jest.spyOn(controllerSpy, 'handle')
-    const request = {
-      body: {
-        name: 'any-name'
-      }
-    }
+    const request = makeRequest()
     await sut.handle(request)
     expect(handleSpy).toBeCalledWith(request)
+  })
+
+  it('should return Controller response', async () => {
+    const { sut } = makeSut()
+    const request = makeRequest()
+    const httpResponse = await sut.handle(request)
+    expect(httpResponse.body).toEqual(request.body)
+    expect(httpResponse.statusCode).toBe(200)
   })
 })
