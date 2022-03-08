e6da82f1abd8b1a029ed27306c00c3a0574d03f5
Bruno Rocha <brunorocha2674@gmail.com>
2022-03-08 12:08:47 -0300

    refactor: move LogRepository creation to a factory

--- src/infra/database/mongodb/LogRepository.spec.ts
+++ src/infra/database/mongodb/LogRepository.spec.ts
@@ -5,6 +5,11 @@ import { env } from '@/main/config'
 const uri = env.MONGO_URL
 let errorCollection: Collection
 
+const makeSut = (): LogRepository => {
+  const sut = new LogRepository()
+  return sut
+}
+
 describe('LogRepository', () => {
   beforeAll(async () => {
     await MongoHelper.connect(uri)
@@ -20,7 +25,7 @@ describe('LogRepository', () => {
   })
 
   it('should register error on success', async () => {
-    const sut = new LogRepository()
+    const sut = makeSut()
     await sut.log('any-stack')
     const count = await errorCollection.countDocuments()
     expect(count).toBe(1)
