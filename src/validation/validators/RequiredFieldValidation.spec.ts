f45c610c5834e48fe99082afd4f0a5f423e4190a
Bruno Rocha <brunorocha2674@gmail.com>
2022-03-09 11:47:17 -0300

    test: ensure RequiredFieldValidation returns null if required field is provided

--- src/validation/validators/RequiredFieldValidation.spec.ts
+++ src/validation/validators/RequiredFieldValidation.spec.ts
@@ -1,4 +1,4 @@
-import { RequiredFieldValidation } from './RequiredFieldValidation'
+import { RequiredFieldValidation } from '@/validation/validators'
 import { MissingParamError } from '@/presentation/errors'
 
 describe('RequiredFieldValidation', () => {
@@ -6,5 +6,11 @@ describe('RequiredFieldValidation', () => {
     const sut = new RequiredFieldValidation('email')
     const error = sut.validate({ name: 'any-name' })
     expect(error).toEqual(new MissingParamError('email'))
+  })
+
+  it('should return null if required field is provided', async () => {
+    const sut = new RequiredFieldValidation('email')
+    const error = sut.validate({ name: 'any-name', email: 'any-email' })
+    expect(error).toBeNull()
   })
 })
