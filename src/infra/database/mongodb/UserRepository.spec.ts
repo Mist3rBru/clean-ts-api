baebb7a1c82e2b65f9372c13fde4124c453bc119
Bruno Rocha <brunorocha2674@gmail.com>
2022-03-10 10:34:32 -0300

    refactor: diference case suits of UserRepository

--- src/infra/database/mongodb/UserRepository.spec.ts
+++ src/infra/database/mongodb/UserRepository.spec.ts
@@ -1,72 +1,74 @@
 import { UserRepository, MongoHelper } from '@/infra/database/mongodb'
 import { AddUserModel } from '@/domain/usecases'
 import { env } from '@/main/config'
+import { Collection } from 'mongodb'
 const uri = env.MONGO_URL
+let usersCollection: Collection
 
 const makeSut = (): UserRepository => {
   const sut = new UserRepository()
   return sut
 }
 
-const makeFakeUser = (): AddUserModel => {
-  return {
-    name: 'any-name',
-    email: 'any-email',
-    password: 'any-password'
-  }
-}
-
-describe('UserRepository add', () => {
-  beforeAll(async () => {
-    await MongoHelper.connect(uri)
-  })
-
-  beforeEach(async () => {
-    const usersCollection = await MongoHelper.getCollection('users')
-    await usersCollection.deleteMany({})
-  })
-
-  afterAll(async () => {
-    await MongoHelper.disconnect()
-  })
-
-  it('should return user when registered on MongoDB', async () => {
-    const sut = makeSut()
-    const model = makeFakeUser()
-    const user = await sut.add(model)
-    expect(user.name).toEqual(model.name)
-    expect(user.email).toEqual(model.email)
-    expect(user.password).toEqual(model.password)
-  })
+const makeFakeUser = (): AddUserModel => ({
+  name: 'any-name',
+  email: 'any-email',
+  password: 'any-password'
 })
 
-describe('UserRepository findByEmail', () => {
-  beforeAll(async () => {
-    await MongoHelper.connect(uri)
+describe('UserRepository', () => {
+  describe('add()', () => {
+    beforeAll(async () => {
+      await MongoHelper.connect(uri)
+    })
+
+    beforeEach(async () => {
+      usersCollection = await MongoHelper.getCollection('users')
+      await usersCollection.deleteMany({})
+    })
+
+    afterAll(async () => {
+      await MongoHelper.disconnect()
+    })
+
+    it('should return user when registered on MongoDB', async () => {
+      const sut = makeSut()
+      const model = makeFakeUser()
+      const user = await sut.add(model)
+      expect(user.name).toEqual(model.name)
+      expect(user.email).toEqual(model.email)
+      expect(user.password).toEqual(model.password)
+    })
   })
 
-  beforeEach(async () => {
-    const usersCollection = await MongoHelper.getCollection('users')
-    await usersCollection.deleteMany({})
-  })
+  describe('findByEmail()', () => {
+    beforeAll(async () => {
+      await MongoHelper.connect(uri)
+    })
 
-  afterAll(async () => {
-    await MongoHelper.disconnect()
-  })
+    beforeEach(async () => {
+      usersCollection = await MongoHelper.getCollection('users')
+      await usersCollection.deleteMany({})
+    })
 
-  it('should return null when user is not found', async () => {
-    const sut = makeSut()
-    const user = await sut.findByEmail('any-email')
-    expect(user).toBeNull()
-  })
+    afterAll(async () => {
+      await MongoHelper.disconnect()
+    })
 
-  it('should return user when user is found', async () => {
-    const sut = makeSut()
-    const model = makeFakeUser()
-    await sut.add(model)
-    const user = await sut.findByEmail(model.email)
-    expect(user.name).toEqual(model.name)
-    expect(user.email).toEqual(model.email)
-    expect(user.password).toEqual(model.password)
+    it('should return null when user is not found', async () => {
+      const sut = makeSut()
+      const user = await sut.findByEmail('any-email')
+      expect(user).toBeNull()
+    })
+
+    it('should return user when user is found', async () => {
+      const sut = makeSut()
+      const model = makeFakeUser()
+      await usersCollection.insertOne(model)
+      const user = await sut.findByEmail(model.email)
+      expect(user.name).toEqual(model.name)
+      expect(user.email).toEqual(model.email)
+      expect(user.password).toEqual(model.password)
+    })
   })
 })
