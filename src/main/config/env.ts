b700a5c1cf45dc7788d3b3a0da23c41c3bdf3e80
Bruno Rocha <brunorocha2674@gmail.com>
2022-03-10 12:53:51 -0300

    chore: setup app for deploy

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export const env = {
-  MONGO_URL: 'mongodb://localhost:27017/clean-ts-api',
-  APP_PORT: 3030,
-  TOKEN_SECRET: 'sXeVB/g57X/YEZOejMEDkQOBufCfUECyS/ZqM6JjcXV/Pwuqc9P5dxtsv9j8SQmZgFxv/GUd5M8cVFAX0/6ijwSFZqM6JjcXV' 
+  MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-ts-api',
+  APP_PORT: process.env.APP_PORT || 3030,
+  TOKEN_SECRET: process.env.TOKEN_SECRET || 'sXeVB/g57X/YEZOejMEDkQOBufCfUECyS/ZqM6JjcXV/Pwuqc9P5dxtsv9j8SQmZgFxv/GUd5M8cVFAX0/6ijwSFZqM6JjcXV' 
 }
