3685cf652ff0259f12ea17ec68e05beb44e4810c
Bruno Rocha <brunorocha2674@gmail.com>
2022-03-12 02:32:34 -0300

    chore: create makeAddSurveyController

---
+++ src/main/composers/controllers/add-survey/add-survey-controller.ts
@@ -0,0 +1,13 @@
+import { SurveyRepository } from '@/infra/database/mongodb'
+import { DbAddSurvey } from '@/data/usecases'
+import { makeAddSurveyValidation, makeLogControllerDecorator } from '@/main/composers'
+import { AddSurveyController } from '@/presentation/controllers'
+import { Controller } from '@/presentation/protocols'
+
+export const makeAddSurveyController = (): Controller => {
+  const addSurveyRepository = new SurveyRepository()
+  const dbAddSurvey = new DbAddSurvey(addSurveyRepository)
+  const validation = makeAddSurveyValidation()
+  const controller = new AddSurveyController(validation, dbAddSurvey)
+  return makeLogControllerDecorator(controller)
+}
