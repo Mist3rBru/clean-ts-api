59d47980fc35ad975b1962d54938085b7ce65fc6
Bruno Rocha <brunorocha2674@gmail.com>
2022-03-12 02:09:08 -0300

    feat: ensure makeAddSurveyValidation receives all validators

---
+++ src/main/composers/controllers/add-survey/add-survey-validation.spec.ts
@@ -0,0 +1,16 @@
+import { makeAddSurveyValidation } from '@/main/composers'
+import { RequiredFieldValidation, ValidationComposite } from '@/validation/validators'
+import { Validation } from '@/validation/protocols'
+
+jest.mock('@/validation/validators/ValidationComposite')
+
+describe('AddSurveyValidation', () => {
+  it('should ', async () => {
+    makeAddSurveyValidation()
+    const validations: Validation[] = []
+    for (const field of ['question', 'answers']) {
+      validations.push(new RequiredFieldValidation(field))
+    }
+    expect(ValidationComposite).toBeCalledWith(validations)
+  })
+})
--- src/main/composers/index.ts
+++ src/main/composers/index.ts
@@ -2,6 +2,7 @@ export * from './controllers/signup/signup-controller'
 export * from './controllers/signup/signup-validation'
 export * from './controllers/login/login-controller'
 export * from './controllers/login/login-validation'
+export * from './controllers/add-survey/add-survey-validation'
 
 export * from './usecases/add-user-composer'
 export * from './usecases/authentication-composer'
