0b379f27283b4342d32b1b9214f5e79a8c6bac00
Bruno Rocha <brunorocha2674@gmail.com>
2022-03-14 15:46:46 -0300

    fix: change property method to shothand method

--- src/presentation/protocols/Middleware.ts
+++ src/presentation/protocols/Middleware.ts
@@ -1,5 +1,5 @@
 import { HttpRequest, HttpResponse } from '@/presentation/protocols'
 
 export interface Middleware {
-  handle: (request: HttpRequest) => Promise<HttpResponse>
+  handle (request: HttpRequest): Promise<HttpResponse>
 }
