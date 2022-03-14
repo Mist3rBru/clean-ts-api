67b8ab4ee08a856036f1515fb92856b926ce4039
Bruno Rocha <brunorocha2674@gmail.com>
2022-03-14 12:13:40 -0300

    chore: adapt to pass throw auth middleware

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -1,14 +1,18 @@
 import { MongoHelper } from '@/infra/database/mongodb'
 import { app, env } from '@/main/config'
+import { sign } from 'jsonwebtoken'
 import { Collection } from 'mongodb'
 import request from 'supertest'
 let surveyCollection: Collection
+let usersCollection: Collection
 
 describe('Survey Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(env.MONGO_URL)
     surveyCollection = await MongoHelper.getCollection('survey')
+    usersCollection = await MongoHelper.getCollection('users')
     await surveyCollection.deleteMany({})
+    await usersCollection.deleteMany({})
   })
 
   afterAll(async () => {
@@ -17,16 +21,29 @@ describe('Survey Routes', () => {
 
   describe('POST /api/survey', () => {
     it('should return 204 on register success', async () => {
+      const { insertedId } = await usersCollection.insertOne({
+        name: 'any-name',
+        email: 'any-email',
+        password: 'any-password',
+        role: 'admin'
+      })
+
+      const token = sign({ id: insertedId }, env.TOKEN_SECRET)
       await request(app)
         .post('/api/survey')
+        .set('authorization', 'Bearer ' + token)
         .send({
           question: 'any-question',
           answers: [{
             image: 'any-image',
             answer: 'any-answer'
+          }, {
+            answer: 'any-answer-again'
           }]
         })
         .expect(204)
+      const dbSurvey = await surveyCollection.findOne({ question: 'any-question' })
+      expect(dbSurvey).toBeTruthy()
     })
   })
 })
