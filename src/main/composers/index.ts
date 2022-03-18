1c8fd5a8d8236fb2a2b194760affef1e35d275cf
Bruno Rocha <brunorocha2674@gmail.com>
2022-03-18 10:52:19 -0300

    chore: move controllers composite to their own folders

--- src/main/composers/index.ts
+++ src/main/composers/index.ts
@@ -1,10 +1,11 @@
-export * from './controllers/signup/signup-controller'
-export * from './controllers/signup/signup-validation'
-export * from './controllers/login/login-controller'
-export * from './controllers/login/login-validation'
-export * from './controllers/add-survey/add-survey-controller'
-export * from './controllers/add-survey/add-survey-validation'
-export * from './controllers/list-surveys/list-surveys'
+export * from './controllers/user/signup/signup-controller'
+export * from './controllers/user/signup/signup-validation'
+export * from './controllers/user/login/login-controller'
+export * from './controllers/user/login/login-validation'
+
+export * from './controllers/survey/add-survey/add-survey-controller'
+export * from './controllers/survey/add-survey/add-survey-validation'
+export * from './controllers/survey/list-surveys/list-surveys'
 
 export * from './middlewares/authentication/auth-middleware'
 export * from './middlewares/authentication/auth-validation'
