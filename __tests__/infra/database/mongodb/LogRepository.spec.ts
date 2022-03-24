c9f8388baa03c0087eb1025ce5a8fc615475f1f7
Bruno Rocha <brunorocha2674@gmail.com>
2022-03-24 10:22:43 -0300

    fix: error db collection to logs db collection

--- __tests__/infra/database/mongodb/LogRepository.spec.ts
+++ __tests__/infra/database/mongodb/LogRepository.spec.ts
@@ -2,7 +2,7 @@ import { LogRepository, MongoHelper } from '@/infra/database/mongodb'
 import { Collection } from 'mongodb'
 import { env } from '@/main/config'
 const uri = env.MONGO_URL
-let errorCollection: Collection
+let logsCollection: Collection
 
 const makeSut = (): LogRepository => {
   const sut = new LogRepository()
@@ -12,11 +12,11 @@ const makeSut = (): LogRepository => {
 describe('LogRepository', () => {
   beforeAll(async () => {
     await MongoHelper.connect(uri)
-    errorCollection = await MongoHelper.getCollection('errors')
+    logsCollection = await MongoHelper.getCollection('logs')
   })
 
   beforeEach(async () => {
-    await errorCollection.deleteMany({})
+    await logsCollection.deleteMany({})
   })
 
   afterAll(async () => {
@@ -26,7 +26,7 @@ describe('LogRepository', () => {
   it('should register error on success', async () => {
     const sut = makeSut()
     await sut.log('any-stack')
-    const count = await errorCollection.countDocuments()
+    const count = await logsCollection.countDocuments()
     expect(count).toBe(1)
   })
 })
--- src/infra/database/mongodb/LogRepository.ts
+++ src/infra/database/mongodb/LogRepository.ts
@@ -3,7 +3,7 @@ import { LogErrorRepository } from '@/data/protocols'
 
 export class LogRepository implements LogErrorRepository {
   async log (stack: string): Promise<void> {
-    const errorCollection = await MongoHelper.getCollection('errors')
-    await errorCollection.insertOne({ stack, date: new Date() })
+    const logsCollection = await MongoHelper.getCollection('logs')
+    await logsCollection.insertOne({ stack, date: new Date() })
   }
 }
