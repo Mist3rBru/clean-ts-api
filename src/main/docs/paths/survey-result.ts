b087874822c98cd9e33af3f9cc958abd6ae04870
Bruno Rocha <brunorocha2674@gmail.com>
2022-03-28 17:01:06 -0300

    docs: create get route

--- src/main/docs/paths/survey-result.ts
+++ src/main/docs/paths/survey-result.ts
@@ -40,8 +40,48 @@ export const surveyResultPath = {
           }
         }
       },
+      400: {
+        $ref: '#/components/badRequest'
+      },
       401: {
         $ref: '#/components/unauthorizedError'
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
+  },
+  get: {
+    security: [{
+      apiKeyAuth: []
+    }],
+    tags: ['Survey Result'],
+    summary: 'route to get survey result',
+    description: 'this route can be used by logged users',
+    parameters: [{
+      in: 'path',
+      name: 'surveyId',
+      required: true,
+      schema: {
+        type: 'string'
+      }
+    }],
+    responses: {
+      200: {
+        description: 'Success',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult'
+            }
+          }
+        }
+      },
+      400: {
+        $ref: '#/components/badRequest'
       },
       403: {
         $ref: '#/components/forbidden'
