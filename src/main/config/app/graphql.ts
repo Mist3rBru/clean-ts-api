2f443681d16738c2fb6752e09305d963e898097d
Bruno Rocha <brunorocha2674@gmail.com>
2022-03-31 16:55:42 -0300

    feat: setup graphql responses

--- src/main/config/app/graphql.ts
+++ src/main/config/app/graphql.ts
@@ -1,13 +1,34 @@
-import { Express } from 'express'
 import { ApolloServer } from 'apollo-server-express'
-import resolvers from '@/main/graphql/resolvers'
 import typeDefs from '@/main/graphql/type-defs'
+import resolvers from '@/main/graphql/resolvers'
+import { GraphQLError } from 'graphql'
 
-export default async (app: Express): Promise<void> => {
-  const server = new ApolloServer({
-    resolvers,
-    typeDefs
-  })
-  await server.start()
-  server.applyMiddleware({ app })
+const checkError = (error: GraphQLError, errorName: string): boolean => {
+  return [error.name, error.originalError?.name].some(name => name === errorName)
 }
+
+const handleErrors = (response: any, errors: readonly GraphQLError[]): void => {
+  errors?.forEach(error => {
+    response.data = undefined
+    if (checkError(error, 'UserInputError')) {
+      response.http.status = 400
+    } else if (checkError(error, 'AuthenticationError')) {
+      response.http.status = 401
+    } else if (checkError(error, 'ForbiddenError')) {
+      response.http.status = 403
+    } else {
+      response.http.status = 500
+    }
+  })
+}
+
+export const setupApolloServer = (): ApolloServer => new ApolloServer({
+  resolvers,
+  typeDefs,
+  context: ({ req }) => ({ req }),
+  plugins: [{
+    requestDidStart: async () => ({
+      willSendResponse: async ({ response, errors }) => handleErrors(response, errors)
+    })
+  }]
+})
