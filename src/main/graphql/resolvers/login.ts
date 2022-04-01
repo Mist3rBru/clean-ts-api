dfd9e5db791f6b2ee979717cf0855251b6521bda
Bruno Rocha <brunorocha2674@gmail.com>
2022-04-01 10:32:26 -0300

    feat: create signUp route

--- src/main/graphql/resolvers/login.ts
+++ src/main/graphql/resolvers/login.ts
@@ -1,8 +1,12 @@
 import { adaptResolver } from '@/main/adapters'
-import { makeLoginController } from '@/main/composers/controllers'
+import { makeLoginController, makeSignUpController } from '@/main/composers/controllers'
 
 export default {
   Query: {
     login: async (parent: any, args: any) => adaptResolver(makeLoginController(), args)
+  },
+
+  Mutation: {
+    signUp: async (parent: any, args: any) => adaptResolver(makeSignUpController(), args)
   }
 }
--- src/main/graphql/type-defs/login.ts
+++ src/main/graphql/type-defs/login.ts
@@ -1,8 +1,12 @@
 import { gql } from 'apollo-server-express'
 
 export default gql`
-  type Query {
+  extend type Query {
     login (email: String!, password: String!): AuthenticationModel!
+  }
+
+  extend type Mutation {
+    signUp (name: String!, email: String!, password: String!, passwordConfirmation: String!): AuthenticationModel!
   }
 
   type AuthenticationModel {
